71cd98f3fc4b96d6023e9590e207ca987278144d
Izie <izzieccampbell@gmail.com>
2017-11-03 21:43:05 -0400

    comment out environment

--- event.js
+++ event.js
@@ -1,12 +1,12 @@
   AFRAME.registerComponent('explode', {
     schema: {
       color: {default: 'red'},
-      preset: {default: 'default'}, 
+      preset: {default: 'default'},
       particleCount: {default: 1000},
     },
     init: function () {
       var data = this.data;
-      var el = this.el;  // <a-box>
+      var el = this.el;
       el.addEventListener('mouseenter', function () {
         console.log('enter');
         el.children[0].setAttribute('particle-system', 'maxParticleCount', 100);
